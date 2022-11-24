c5b84f85a4ff8cc46965fa3e3b4ff605f26f8de5
Alihan KUZUCUK <95879997+alihankuzucuk@users.noreply.github.com>
2022-11-24 15:49:31 +0300

    Interface bug fixed

--- BreakingBad/BreakingBad/Controllers/BaseViewController.swift
+++ BreakingBad/BreakingBad/Controllers/BaseViewController.swift
@@ -18,7 +18,7 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupActivityIndicatorView()
-        prepareView()
+        // prepareView() // Causes some interface bugs with Xcode 14.1
     }
     
     // MARK: - Methods
